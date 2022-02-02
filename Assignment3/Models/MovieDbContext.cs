646341b34a98cd0b9c3af0f6e313debb8c9250c4
Michael Zaar <michael.sh.zaar@gmail.com>
2022-02-02 21:34:33 +0100

    feat: added basic controllers

--- Assignment3/Models/MovieDbContext.cs
+++ Assignment3/Models/MovieDbContext.cs
@@ -19,40 +19,41 @@ namespace Assignment3.Models
         {
             // Seed data
             modelBuilder.Entity<Character>()
-                .HasData(new Character { Id = 1, Name = "Robert Balboa", Alias = "Rocky", Gender = "Male", Photo = "https://en.wikipedia.org/wiki/File:Rocky_balboa.jpeg" });
+                .HasData(new Character { Id = 1, Name = "Robert Balboa", Alias = "Rocky", Gender = "Male", Photo = "https://upload.wikimedia.org/wikipedia/en/5/53/Rocky_balboa.jpeg" });
             modelBuilder.Entity<Character>()
-                .HasData(new Character { Id = 2, Name = "Adriana Pennino", Alias = "Adrian", Gender = "Female", Photo = "https://en.wikipedia.org/wiki/File:Talia_Shire_1976_edit.JPG" });
+                .HasData(new Character { Id = 2, Name = "Adriana Pennino", Alias = "Adrian", Gender = "Female", Photo = "https://upload.wikimedia.org/wikipedia/commons/c/cd/Talia_Shire_1976_edit.JPG" });
             modelBuilder.Entity<Character>()
-                .HasData(new Character { Id = 3, Name = "Michael Goldmill", Alias = "Mickey", Gender = "Male", Photo = "https://en.wikipedia.org/wiki/File:Mickey_Goldmill.jpg" });
+                .HasData(new Character { Id = 3, Name = "Michael Goldmill", Alias = "Mickey", Gender = "Male", Photo = "https://upload.wikimedia.org/wikipedia/en/7/7b/Mickey_Goldmill.jpg" });
 
             modelBuilder.Entity<Movie>()
-                .HasData(new Movie { Id = 1, Title = "Rocky", Genre = "Sports Drama", Director = "John G. Avildsen", ReleaseYear = 1976, Picture = "", Trailer = "", FranchiseId = 1});
+                .HasData(new Movie { Id = 1, Title = "Rocky", Genre = "Sports Drama", Director = "John G. Avildsen", ReleaseYear = 1976, FranchiseId = 1});
             modelBuilder.Entity<Movie>()
-               .HasData(new Movie { Id = 2, Title = "Rocky II", Genre = "Sports Drama", Director = "Sylvester Stallone", ReleaseYear = 1979, Picture = "", Trailer = "", FranchiseId = 1 });
+               .HasData(new Movie { Id = 2, Title = "Rocky II", Genre = "Sports Drama", Director = "Sylvester Stallone", ReleaseYear = 1979, FranchiseId = 1 });
             modelBuilder.Entity<Movie>()
-               .HasData(new Movie { Id = 3, Title = "Rocky III", Genre = "Sports Drama", Director = "Sylvester Stallone", ReleaseYear = 1982, Picture = "", Trailer = "", FranchiseId = 1 });
+               .HasData(new Movie { Id = 3, Title = "Rocky III", Genre = "Sports Drama", Director = "Sylvester Stallone", ReleaseYear = 1982, FranchiseId = 1 });
 
             modelBuilder.Entity<Franchise>()
-                .HasData(new Franchise { Id = 1, Name = "Rocky", Description = "" });
+                .HasData(new Franchise { Id = 1, Name = "Rocky", Description = "Movie about you get knocked down and you get back up again" });
             modelBuilder.Entity<Franchise>()
-                .HasData(new Franchise { Id = 2, Name = "Lord of the Rings", Description = "" });
+                .HasData(new Franchise { Id = 2, Name = "Lord of the Rings", Description = "Movie about some shorties carrying some jewelry" });
             modelBuilder.Entity<Franchise>()
-                .HasData(new Franchise { Id = 3, Name = "Harry Potter", Description = "" });
+                .HasData(new Franchise { Id = 3, Name = "Harry Potter", Description = "Movie 'bout some wizard bloke" });
 
             // Seed m2m
             modelBuilder.Entity<Movie>()
                 .HasMany(m => m.Characters)
-                .WithMany(p => p.Movies)
+                .WithMany(c => c.Movies)
                 .UsingEntity<Dictionary<string, object>>(
                 "MovieCharacters",
-                r => r.HasOne<Character>().WithMany().HasForeignKey("CharacterId"),
-                l => l.HasOne<Movie>().WithMany().HasForeignKey("MovieId"),
-                je =>
+                right => right.HasOne<Character>().WithMany().HasForeignKey("CharacterId"),
+                left => left.HasOne<Movie>().WithMany().HasForeignKey("MovieId"),
+                join =>
                 {
-                    je.HasKey("CharacterId", "MovieId");
-                    je.HasData(
+                    join.HasKey("CharacterId", "MovieId");
+                    join.HasData(
                         new { CharacterId = 1, MovieId = 1 },
-                        new { CharacterId = 2, MovieId = 1 }
+                        new { CharacterId = 2, MovieId = 1 },
+                        new { CharacterId = 3, MovieId = 2 }
                         );
                 });
         }
--- Assignment3/Startup.cs
+++ Assignment3/Startup.cs
@@ -28,8 +28,8 @@ namespace Assignment3
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<MovieDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddDbContext<MovieDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Assignment3", Version = "v1" });
